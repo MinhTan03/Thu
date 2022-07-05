4bb061de95c7c6fab0e021f68c42bd23a441fc45
TanTrinh <104217824+MinhTan03@users.noreply.github.com>
2022-07-06 00:28:33 +0700

    Add files via upload
    Create Card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em yêu ❤️';
+const giftUrl = 'https://www.facebook.com/kimsthus.luongs';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Em yêu';
+const contentCard = 'Chúc honey của anh có một sinh nhật tràn ngập niềm vui và tiếng cười. Chúc một nửa của cuộc đời anh luôn cảm thấy hạnh phúc khi có anh ở bên. Cho dù em đi tới đâu anh vẫn dõi theo từng bước chân em. Anh chỉ mong được theo em trên đoạn đường còn lại, được dìu em khi em ngã, hạnh phúc khi được thấy em cười, và cạnh em khi em cơ đơn nhất. Với anh như thế là đủ. Mãi yêu Em ❤️';
+const giftImage = '49.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
+
